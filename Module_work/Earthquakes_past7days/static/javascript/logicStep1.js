ed0adafe1949f4ebd39dd57c8531122b4dd6599a
Joshua Rhodes <jdw.rhodes@gmail.com>
2021-04-02 11:21:52 -0500

    added earthquakes

--- Module_work/Earthquakes_past7days/static/javascript/logicStep1.js
+++ Module_work/Earthquakes_past7days/static/javascript/logicStep1.js
@@ -11,7 +11,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
-// We create the dark view tile layer that will be an option for our map.
+// We create the Satellite Street view tile layer that will be an option for our map.
 let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -26,30 +26,17 @@ let baseMaps = {
 
 // Create a map object with center, zoom level and default layer. This is the more verbose way of creating a map as compared to just using setView()
 let map = L.map('mapid', {
-    center: [43.7, -79.3],
+    center: [39.5, -98.5],
     zoom: 3,
     layers: [streets]
-})
-
-// Accessing the Toronto neighborhoods GeoJSON URL. Having this after the titleLayer ensures that the map gets loaded before the data is added to it
-let torontoHoods = 'https://raw.githubusercontent.com/jdwrhodes/Mapping_Earthquakes/main/torontoNeighborhoods.json'
-
-// Create a style for the lines.
-let myStyle = {
-    color: "#ffffa1",
-    weight: 2
-}
+});
 
 // Grabbing our GeoJSON data
-d3.json(torontoHoods).then(function(data) {
-    console.log(data);
+d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson').then(function(data) {
 
     // Creating a GeoJSON layer with the retrieved data
     L.geoJson(data).addTo(map);
 });
-
-// Then we add our 'graymap' tile layer to the map.
-//baseMaps.addTo(map);
 
 // Pass our map layers into our layers control and add the layers control to the map. By using "Control," it allows the use of multiple layers/styles. 
 L.control.layers(baseMaps).addTo(map);
